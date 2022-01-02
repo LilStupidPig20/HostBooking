36ed57c1a8da331c818b86bea562199cdf5e05f9
fateful1 <facelessasha@yandex.ru>
2022-01-02 19:41:22 +0500

    готовая авторизация

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,17 +1,23 @@
 import React from "react";
 import { Navbar } from "./components/Navbar";
 import { useRoutes } from "./routes";
+import {useAuth} from "./hooks/auth.hook"
 import { BrowserRouter as Router } from "react-router-dom";
+import { AuthContext } from "./context/AuthContext";
 
 export const App = () => {
-  const routes = useRoutes(true);
+  const { token, login, logout } = useAuth()
+  const isAuthenticated = !!token
+  const routes = useRoutes(isAuthenticated)
 
   return (
-    <div>
+    <AuthContext.Provider value={{
+      token, login, logout, isAuthenticated
+    }} >
       <Navbar />
       <Router>
         {routes}
       </Router>
-    </div>
+      </AuthContext.Provider>
   );
 };
--- ClientApp/src/pages/BookingPage/index.js
+++ ClientApp/src/pages/BookingPage/index.js
@@ -1,24 +1,67 @@
-import React from 'react'
-import { Map } from '../../components/Map'
-import styles from "./booking.module.css"
+import React, { useState,useContext } from "react";
+import { Map } from "../../components/Map";
+import { AuthContext } from "../../context/AuthContext";
+import { useHttp } from "../../hooks/http.hook";
+import styles from "./booking.module.css";
+import DatePicker, { registerLocale } from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import ru from "date-fns/locale/ru";
+registerLocale("ru", ru);
 
 export const BookingPage = () => {
-    return (
-        <div className={styles.bookingContainer}>
-            <div className={styles.controlPanel}>s</div>
-            <div className={styles.bookingSide}>
-                <div className={styles.map}>
-                    <Map/>
-                </div>
-            </div>
-            
+  const auth = useContext(AuthContext);
 
+  const { loading, error, clearError, request } = useHttp();
+  const [date, setDate] = useState(new Date());
+  const [startTime, setStartTime] = useState(date.getTime());
+  const [endTime, setEndTime] = useState(date.getTime());
 
-
-
-
-
-
-        </div>
-    )
+const testHandler= async ()=>{
+  try {
+    const data = await request("/api/table/login", "POST",{},{Authorization: `Bearer ${auth.token}`});
+  } catch (error) {console.log(error)}
 }
+
+  return (
+    <div className={styles.bookingContainer}>
+      <div className={styles.controlPanel}>
+        Выберите дату
+        <DatePicker
+          dateFormat="dd.MM.yy"
+          locale={"ru"}
+          selected={date}
+          onChange={(date) => setDate(date)}
+        />
+        Выберите время
+        <DatePicker
+          selected={startTime}
+          onChange={(startTime)=>{setStartTime(startTime)}}
+          showTimeSelect
+          showTimeSelectOnly
+          timeIntervals={15}
+          timeCaption="Time"
+          timeFormat="HH:mm"
+          dateFormat="HH:mm"
+        />
+        До скольки вы хотите забронировать
+        <DatePicker
+          selected={endTime}
+          onChange={(endTime)=>{setEndTime(endTime)}}
+          showTimeSelect
+          showTimeSelectOnly
+          timeIntervals={15}
+          timeCaption="Time"
+          timeFormat="HH:mm"
+          dateFormat="HH:mm"
+        />
+        <button className={styles.button}>Показать свободные столы</button>
+        <button className={styles.buttonSubmit} onClick={testHandler}>Забронировать</button>
+      </div>
+      <div className={styles.bookingSide}>
+        <div className={styles.map}>
+          <Map />
+        </div>
+      </div>
+    </div>
+  );
+};
