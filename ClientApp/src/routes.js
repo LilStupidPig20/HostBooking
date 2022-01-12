1e6f6c88fa130a135b8908331c24cc68f31a0a85
fateful1 <facelessasha@yandex.ru>
2022-01-12 15:28:49 +0500

    доделал страницу логина

--- ClientApp/src/routes.js
+++ ClientApp/src/routes.js
@@ -8,6 +8,7 @@ export const useRoutes = (isAuthenticated) => {
     return (
       <Switch>
         <Route path="/booking" exact>
+          <Navbar />
           <BookingPage />
         </Route>
         {/* <Route path='*' exact={true} component={My404Component} /> */}
